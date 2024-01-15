02c219c593a8c4ffec58c46e9bb64eb07b1f4b30
Abylay Kuvatov <abukuvatov@icloud.com>
2024-01-15 16:18:10 +0600

    Changed tortoise conf file and added migrations folder

--- src/app/db/conf.py
+++ src/app/db/conf.py
@@ -1,4 +1,16 @@
-from core.settings import database_url
+try:
+    from core.settings import database_url
+except ImportError:
+    from src.app.core.settings import database_url
+    
+
+def get_tortoise_models_path():
+    try:
+        import db.models
+        return 'db.models'
+    except ImportError:
+        import src.app.db.models
+        return 'src.app.db.models'
 
 
 TORTOISE_ORM = {
@@ -7,7 +19,7 @@ TORTOISE_ORM = {
     },
     'apps': {
         'models': {
-            'models': ['aerich.models', 'db.models'],
+            'models': ['aerich.models', get_tortoise_models_path()],
             'default_connection': 'default',
         },
     },
---
+++ src/migrations/models/1_20240115101703_None.py
@@ -0,0 +1,31 @@
+from tortoise import BaseDBAsyncClient
+
+
+async def upgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        CREATE TABLE IF NOT EXISTS "aerich" (
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "version" VARCHAR(255) NOT NULL,
+    "app" VARCHAR(100) NOT NULL,
+    "content" JSONB NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "subject" (
+    "id" UUID NOT NULL  PRIMARY KEY,
+    "created_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+    "name" VARCHAR(255) NOT NULL,
+    "description" TEXT
+);
+CREATE TABLE IF NOT EXISTS "material" (
+    "id" UUID NOT NULL  PRIMARY KEY,
+    "created_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL  DEFAULT CURRENT_TIMESTAMP,
+    "topic" VARCHAR(255) NOT NULL,
+    "content" TEXT,
+    "subject_id" UUID REFERENCES "subject" ("id") ON DELETE CASCADE
+);"""
+
+
+async def downgrade(db: BaseDBAsyncClient) -> str:
+    return """
+        """
